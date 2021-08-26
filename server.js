06a7fd8d1fb2fce1d38d8c82be267d3321350e2c
chrisvanetten <christophervanetten@gmail.com>
2021-08-26 15:28:24 -0500

    added Workout.js, added get & post routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const mongoose = require("mongoose");
+const { Workout } = require("./models");
 
 const app = express()
 const PORT = process.env.PORT || 3001;
@@ -7,6 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(express.static("public"))
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 //routing
 
@@ -16,6 +23,18 @@ app.get('/stats', (req, res) => {
 
 app.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/exercise.html"))
+})
+
+app.get('/api/workouts', (req, res) => {
+    Workout.find({}).then(data => res.json(data))
+})
+
+app.get('/api/workouts/range', (req, res) => {
+    Workout.find({}).then(data => res.json(data))
+})
+
+app.post('/api/workouts', (req, res) => {
+    Workout.create({}).then(data => res.json(data))
 })
 
 app.listen(PORT)
